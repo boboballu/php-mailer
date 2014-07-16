412b49c3228a989f8cbca24001971889152e74e5
Tarun govind <tarun.835081@gmail.com>
2014-07-16 11:51:20 +0530

    MAILER

---
+++ advmlr.php
@@ -0,0 +1,20 @@
+<title> bobo PHP mailer </title>
+<h1> Fastest PHP mailer by Tarun Gvoind(bobo) - ADVANCED</h1>
+<form method="GET" action="bobomailer.php">
+FROM     : <input name="from" size=20 type="text"/><br>
+TO       : <input name="to" size=20 type="text"/><br>
+SUBJECT  : <input name="subject" size=20 type="text"/><br>
+MESSAGE  : <br><textarea cols="20" rows="20" name="msg"></textarea>
+<?php
+$to = $_GET['to'];
+$from = $_GET['from'];
+$subj = $_GET['subject'];
+$msg = $_GET['msg'];
+$header = "From: ".$from;
+$mail = mail($to,$subj,$msg,$header);
+if($mail) {
+echo '<br>Email sent<br>';
+}
+?>
+<input type="submit" name="submit" value="enter"/>
+</form>
